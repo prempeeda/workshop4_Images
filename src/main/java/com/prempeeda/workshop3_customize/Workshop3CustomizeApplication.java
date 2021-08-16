5fecc0de0b6b6531882bca9e6537a647bec9bd95
prempeeda <prem25122549@gmail.com>
2021-08-16 14:50:37 +0700

    first commit

---
+++ src/main/java/com/prempeeda/workshop3_customize/Workshop3CustomizeApplication.java
@@ -0,0 +1,13 @@
+package com.prempeeda.workshop3_customize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop3CustomizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop3CustomizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prempeeda/workshop3_customize/Workshop3CustomizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prempeeda.workshop3_customize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop3CustomizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
